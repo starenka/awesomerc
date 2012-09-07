9098c63fbffe7a07ba9909e5653b418af1d34ce5
starenka <starenka0@gmail.com>
2012-09-07 23:58:47 +0200

    more luv to menu

--- themes/starenka/theme.lua
+++ themes/starenka/theme.lua
@@ -70,8 +70,8 @@ theme.mouse_finder_color = "#CC9393"
 -- Variables set for theming the menu:
 -- menu_[bg|fg]_[normal|focus]
 -- menu_[border_color|border_width]
-theme.menu_height = "15"
-theme.menu_width  = "100"
+theme.menu_height = "16"
+theme.menu_width  = "160"
 -- }}}
 
 -- {{{ Icons
