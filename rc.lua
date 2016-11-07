78a64bf30c82e2faaeb8fe7d14cfe0531dd2388a
starenka <starenka0@gmail.com>
2016-11-07 15:36:31 +0100

    playpause not pause

--- rc.lua
+++ rc.lua
@@ -391,11 +391,11 @@ globalkeys = awful.util.table.join(awful.key({ modkey, }, "Left", awful.tag.view
   awful.key({ modkey, shiftkey }, "Left", function() awful.util.spawn("qdbus " .. clementine_dbus .. " org.freedesktop.MediaPlayer.Previous") end),
   awful.key({ modkey, shiftkey }, "Right", function() awful.util.spawn("qdbus " .. clementine_dbus .. " org.mpris.MediaPlayer2.Player.Next") end),
   awful.key({ modkey, shiftkey }, "Up", function() awful.util.spawn("qdbus " .. clementine_dbus .. " org.mpris.MediaPlayer2.Player.Play") end),
-  awful.key({ modkey, shiftkey }, "Down", function() awful.util.spawn("qdbus " .. clementine_dbus .. " org.mpris.MediaPlayer2.Player.Pause") end),
+  awful.key({ modkey, shiftkey }, "Down", function() awful.util.spawn("qdbus " .. clementine_dbus .. " org.mpris.MediaPlayer2.Player.PlayPause") end),
   awful.key({}, "XF86AudioPrev", function() awful.util.spawn("qdbus " .. clementine_dbus .. " org.mpris.MediaPlayer2.Player.Previous") end),
   awful.key({}, "XF86AudioNext", function() awful.util.spawn("qdbus " .. clementine_dbus .. " org.mpris.MediaPlayer2.Player.Next") end),
   awful.key({}, "XF86AudioPlay", function() awful.util.spawn("qdbus " .. clementine_dbus .. " org.mpris.MediaPlayer2.Player.Play") end),
-  awful.key({}, "XF86AudioStop", function() awful.util.spawn("qdbus " .. clementine_dbus .. " org.mpris.MediaPlayer2.Player.Pause") end),
+  awful.key({}, "XF86AudioStop", function() awful.util.spawn("qdbus " .. clementine_dbus .. " org.mpris.MediaPlayer2.Player.PlayPause") end),
   
   -- misc
   awful.key({ altkey, }, "space", function() mypromptbox[mouse.screen]:run() end), --launcher
