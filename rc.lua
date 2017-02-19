ac21a035ac68f35d58041be9894ce1f19ec04842
starenka <starenka0@gmail.com>
2017-02-19 23:36:32 +0100

    biggery battery polling interval

--- rc.lua
+++ rc.lua
@@ -172,11 +172,10 @@ vicious.register(gmailwidget, vicious.widgets.gmail, '<span font-weight="bold">$
 
 -- Battery
 battery = require('battery')
-battery_poll_int = 7 --seconds
+battery_poll_int = 13 --seconds
 
 -- you can override default battery settings here
 -- battery.settings={method='generic', color='#dcdccc', battery='BAT0', warning={ color='#fecf35', level=30}, critical={color='red', level=15}}
-battery.settings.method='acpi'
 batterywidget = {
   widget = wibox.widget.textbox(),
   timer = timer({ timeout = battery_poll_int })
