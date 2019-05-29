8455247c3e7907fafdff269d8acf95167585029b
starenka <starenka0@gmail.com>
2019-05-29 23:23:25 +0200

    change fonts

--- themes/starenka/theme.lua
+++ themes/starenka/theme.lua
@@ -7,9 +7,10 @@ theme = {}
 local THEME_DIR = string.format("%s/.config/awesome/themes/starenka/", os.getenv("HOME"))
 
 -- {{{ Styles
-theme.font      = "andale mono 8.5"
+--theme.font      = "andale mono 8.5"
 --theme.font      = "dejavu sans mono 8"
---theme.font      = "monospace 8"
+--theme.font      = "monospace 7.3"
+theme.font      = "monospace 8.5"
 
 -- {{{ Colors
 theme.fg_normal = "#DCDCCC"
