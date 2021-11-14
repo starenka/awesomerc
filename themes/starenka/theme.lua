541223f321f8a26be74fc5b1aca999cba6cd3191
starenka <starenka@kosmik1>
2021-11-14 17:13:10 +0100

    tune up theme

--- themes/starenka/theme.lua
+++ themes/starenka/theme.lua
@@ -9,7 +9,8 @@ local THEME_DIR = string.format("%s/.config/awesome/themes/starenka/", os.getenv
 -- {{{ Styles
 --theme.font      = "andale mono 8.5"
 --theme.font      = "dejavu sans mono 9"
-theme.font      = "monospace 7.5"
+--theme.font      = "monospace 7.5"
+--theme.font      = "ubuntu mono 9.3"
 theme.font      = "hack 8.2"
 
 -- {{{ Colors
@@ -83,7 +84,7 @@ theme.taglist_squares_unsel = THEME_DIR .. "taglist/squarefz.png"
 -- {{{ Misc
 theme.awesome_icon           = THEME_DIR .. "debian-icon.png"
 theme.menu_submenu_icon      = "/usr/share/awesome/themes/default/submenu.png"
-theme.tasklist_font = "hack 8.5"
+theme.tasklist_font = "hack 8.2"
 theme.tasklist_disable_icon = true
 
 --theme.tasklist_bg_focus = '#FFFFFF'
