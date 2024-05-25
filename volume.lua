7b0242cbe73ad84b92771fdc49710a7f7758b550
starenka <starenka0@gmail.com>
2024-05-26 00:06:52 +0200

    fix volume stuff (pulseaudio via pipewire)

--- volume.lua
+++ volume.lua
@@ -1,31 +1,18 @@
 -- https://raw.githubusercontent.com/esn89/volumetextwidget/master/textvolume.lua
 
 local wibox = require("wibox")
-local awful = require("awful")
 local gears = require("gears")
 
 volume_widget = wibox.widget.textbox()
 volume_widget:set_align("right")
 
+HOME = os.getenv("HOME")
 function update_volume(widget)
-   local fd = io.popen("amixer sget Master")
+   local fd = io.popen(string.format("%s/bin/volume level", HOME))
    local status = fd:read("*all")
    fd:close()
-
-   local volume = string.match(status, "(%d?%d?%d)%%")
-   volume = string.format("% 3d", volume)
-
-   status = string.match(status, "%[(o[^%]]*)%]")
-
-   if string.find(status, "on", 1, true) then
-   -- For the volume number percentage
-       volume = volume .. "%"
-   else
-   -- For displaying the mute status.
-       volume = volume .. "M"
-
-   end
-   widget:set_markup(' <span font-size="small">VOL ' .. volume .. '</span> ')
+   
+   widget:set_markup(' <span font-size="small">VOL ' .. status .. '</span> ')
 end
 
 update_volume(volume_widget)
