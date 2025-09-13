9f67df315aa2b136ffc21d702c59386abd2854e8
starenka <starenka0@gmail.com>
2025-09-13 22:48:33 +0200

    dbeaver

--- rc.lua
+++ rc.lua
@@ -690,6 +690,7 @@ awful.rules.rules = {
     {rule = {class = "Unciv"}, properties = {screen = 1, tag = tags[7]}},
     {rule = {class = "Civ5XP"}, properties = {screen = 1, tag = tags[7]}},
     {rule = {class = "TuxPaint.TuxPaint"}, properties = {screen = 1, tag = tags[7]}},
+    {rule = {class = "DBeaver"}, properties = {screen = 1, tag = tags[7]}},
 
 }
 -- }}}
